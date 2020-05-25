87ed6cde619cbae260f6a1eb1e6482813ccd9689
Cal-Hagner <caleb.hagner@pm.me>
2020-05-25 16:57:59 -0400

    npm run format

--- __tests__/network-check.test.ts
+++ __tests__/network-check.test.ts
@@ -1,7 +1,7 @@
-const { isNetworkConnected } = require("../src/struct/index");
+const { isNetworkConnected } = require('../src/struct/index')
 
-test("makes sure that we can tell that our unit tests run somewhere with Internet connection", async () => {
-  expect(await isNetworkConnected()).toBe(true);
-});
+test('makes sure that we can tell that our unit tests run somewhere with Internet connection', async () => {
+  expect(await isNetworkConnected()).toBe(true)
+})
 
-export {};
+export {}
