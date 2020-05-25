70152ef277868afdb7f655543f275294c5b06b15
Cal-Hagner <caleb.hagner@pm.me>
2020-05-25 16:54:52 -0400

    Correct network-check unit testing to proper folder

---
+++ __tests__/network-check.test.ts
@@ -0,0 +1,7 @@
+const { isNetworkConnected } = require("../src/struct/index");
+
+test("makes sure that we can tell that our unit tests run somewhere with Internet connection", async () => {
+  expect(await isNetworkConnected()).toBe(true);
+});
+
+export {};
--- src/tests/network-check.test.js
+++
@@ -1,5 +0,0 @@
-const { isNetworkConnected } = require('../struct/index')
-
-test('makes sure that we can tell that our unit tests run somewhere with Internet connection', async () => {
-  expect(await isNetworkConnected()).toBe(true)
-})
