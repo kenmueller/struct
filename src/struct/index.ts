5503a3b5781e700faf7a94241e897944b4f33596
Cal-Hagner <caleb.hagner@pm.me>
2020-05-27 11:58:28 -0400

    define isNetworkConnected as async

--- src/struct/index.ts
+++ src/struct/index.ts
@@ -1,53 +1,53 @@
-import { connect } from 'http2'
-import axios from 'axios'
-import { Readable } from 'stream'
+import { connect } from "http2";
+import axios from "axios";
+import { Readable } from "stream";
 
 /**
  * Returns whether or not the host computer has a network connection.
  */
-export function isNetworkConnected(): Promise<boolean> {
-  return new Promise(resolve => {
-    const client = connect('https://www.google.com') // Google is a nice standard site to check network connection
-    client.on('connect', () => {
-      resolve(true)
-      client.destroy()
-    })
-    client.on('error', () => {
-      resolve(false)
-      client.destroy()
-    })
-  })
+export async function isNetworkConnected(): Promise<boolean> {
+  return new Promise((resolve) => {
+    const client = connect("https://www.google.com"); // Google is a nice standard site to check network connection
+    client.on("connect", () => {
+      resolve(true);
+      client.destroy();
+    });
+    client.on("error", () => {
+      resolve(false);
+      client.destroy();
+    });
+  });
 }
 
 export async function listStructures(
-  registry: string = 'Standard-Structure/Standard-Structure'
+  registry: string = "Standard-Structure/Standard-Structure",
 ): Promise<{
-  frameworks: string[]
-  languages: string[]
+  frameworks: string[];
+  languages: string[];
 }> {
   const [frameworks, languages] = (
     await Promise.all([
       axios(`https://api.github.com/repos/${registry}/contents/frameworks`),
-      axios(`https://api.github.com/repos/${registry}/contents/languages`)
+      axios(`https://api.github.com/repos/${registry}/contents/languages`),
     ])
   )
-    .map(resp => resp.data)
-    .map(i => {
-      return i.filter(x => x.type === 'dir').map(x => x.name)
-    })
+    .map((resp) => resp.data)
+    .map((i) => {
+      return i.filter((x) => x.type === "dir").map((x) => x.name);
+    });
 
   return {
     frameworks,
-    languages
-  }
+    languages,
+  };
 }
 
 export async function getAllStructures(
-  registry: string = 'Standard-Structure/Standard-Structure'
+  registry: string = "Standard-Structure/Standard-Structure",
 ): Promise<Readable> {
   const stream = await axios.get(
     `https://api.github.com/repos/${registry}/tarball`,
-    { responseType: 'stream' }
-  )
-  return stream.data
+    { responseType: "stream" },
+  );
+  return stream.data;
 }
